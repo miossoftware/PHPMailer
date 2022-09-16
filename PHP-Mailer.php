50c438d5f761eed0dc4a6707ce9b6281e8b2eccc
miossoftware <96083556+miossoftware@users.noreply.github.com>
2022-09-16 22:05:36 +0300

    First Commit
    Amacım PHPMailer dosyanızı projenize dahil ettiğinizi varsayarak geriye kalan bölümde ne yapmanız gerektiğini göstermektir

---
+++ PHP-Mailer.php
@@ -0,0 +1,40 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../PHPMailer/src/Exception.php';
+require '../PHPMailer/src/PHPMailer.php';
+require '../PHPMailer/src/SMTP.php';
+if ($_POST){
+    $mail = new PHPMailer();
+
+    $mail->isSMTP();
+    $mail->SMTPKeepAlive = true;
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = 'tls';
+
+    $mail->Port = 587;
+    $mail->Host = 'smtp.gmail.com';
+
+    $mail->Username = 'smtkbdy5422@gmail.com';
+    $mail->Password = 'Sa12345Sa';
+
+    $mail->CharSet = 'utf-8';
+    $mail->setFrom($_POST['name'], $_POST['email']);
+    $mail->addAddress("smtkbdy5422@gmail.com", "Samet Kabadayı");
+
+
+    $mail->isHTML(true);
+    $mail->Subject = $_POST['name'] .' tarafından bir mesajınız var';
+    $mail->Body = $_POST['subject'] . '<br>' . $_POST['message'] . '<br>'.'mail adresi: '.$_POST['email'];
+
+    $mail->addAttachment($_POST['message']);
+    if ($mail->send()) {
+        echo 'Mail Gönderim İşlemi Başarılı';
+    } else {
+        echo 'Gönderim sırasında bir sorun oluştu';
+    }
+
+}
+?>
